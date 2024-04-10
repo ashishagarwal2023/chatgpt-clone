852b778db7c6292b3f27a6ea1e922a88732519c7
Ashish Agarwal <code.with.aasheesh@gmail.com>
2024-04-10 21:42:44 +0530

    fix img

--- src/app/components/Chat.jsx
+++ src/app/components/Chat.jsx
@@ -48,7 +48,7 @@ export const Chat = () => {
             <div className="no-scrollbar mt-16 pb-12 overflow-hidden ">
           <div className="flex flex-row justify-center">
             <Image
-              src="/img/gpt.svg"
+              src="./img/gpt.svg"
               alt="ChatGPT"
               width={50}
               className="rounded-full p-1 text-white bg-black border shadow-md shadow-white inline-flex invert"
@@ -65,7 +65,7 @@ export const Chat = () => {
             <div key={index} className="mt-4">
               {msg.isUser ? (
                 <Image
-                  src="/img/user.png"
+                  src="./img/user.png"
                   alt="User"
                   width={25}
                   height={25}
@@ -73,7 +73,7 @@ export const Chat = () => {
                 />
               ) : (
                 <Image
-                  src="/img/gpt.svg"
+                  src="./img/gpt.svg"
                   alt="ChatGPT"
                   width={25}
                   className="rounded-full p-1 text-white bg-[#19c37d] inline-flex"
