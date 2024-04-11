383a86a7969247d7fcdfeb44494cc40052ea2ee2
Ashish Agarwal <code.with.aasheesh@gmail.com>
2024-04-11 15:40:16 +0530

    update images to streamline development

--- src/app/components/Chat.jsx
+++ src/app/components/Chat.jsx
@@ -1,5 +1,9 @@
 "use client";
 
+// Images
+import user from "/public/img/user.png";
+import gpt from "/public/img/gpt.svg";
+
 import Image from "next/image";
 import { useState } from "react";
 
@@ -53,7 +57,7 @@ export const Chat = () => {
           <div className="no-scrollbar mt-16 pb-12 overflow-hidden ">
             <div className="flex flex-row justify-center">
               <Image
-                src="./img/gpt.svg"
+                src={gpt}
                 alt="ChatGPT"
                 width={50}
                 className="rounded-full p-1 text-white bg-black border shadow-md shadow-white inline-flex invert"
@@ -78,7 +82,7 @@ export const Chat = () => {
             <div key={index} className="mt-4">
               {msg.isUser ? (
                 <Image
-                  src="./img/user.png"
+                  src={user}
                   alt="User"
                   width={25}
                   height={25}
@@ -86,7 +90,7 @@ export const Chat = () => {
                 />
               ) : (
                 <Image
-                  src="./img/gpt.svg"
+                  src={gpt}
                   alt="FreeGPT"
                   width={25}
                   className="rounded-full p-1 text-white bg-[#19afc3] inline-flex"
